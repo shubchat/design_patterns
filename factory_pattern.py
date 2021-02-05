7d58bfcdfe3eaa938d5bf7324972caed357370e0
shubchat <shub.chat@gmail.com>
2021-02-06 05:51:29 +1100

    factory pattern

--- factory_pattern.py
+++ factory_pattern.py
@@ -1,41 +1,42 @@
 # A factory method lets subclasses decide the concrete implementation of a function
-#or a product
+# or a product
 
 # Below is an example of different pizza stores of a single pizza outlet
 
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
-class PizzaStore(ABC):->'ABC for creating a pizza store'
 
+class PizzaStore(ABC):
     @abstractmethod
     def create_pizza():
         pass
 
+
 class ChicagoPizzaStore(PizzaStore):
-    name='Chicago_Pizza'
-    def __init__(self,type):
-        self.type=type
+    name = "Chicago_Pizza"
+
+    def __init__(self, type):
+        self.type = type
 
     def create_pizza(self):
-        return Chicago_Pizza(self.type)
+        return ChicagoPizza(self.type)
 
-    
+
 class NYPizzaStore(PizzaStore):
-    name='NY_Pizza'
-    def __init__(self,type):
-        self.type=type
-    
-    def create_pizza(self):
-        return NY_Pizza(self.type)
+    name = "NY_Pizza"
 
-    
+    def __init__(self, type):
+        self.type = type
+
+    def create_pizza(self):
+        return NYPizza(self.type)
+
 
 class Pizza(ABC):
-    
     @abstractmethod
     def cheese_pizza():
         pass
-    
+
     @abstractmethod
     def pepperoni_pizza():
         pass
@@ -43,42 +44,79 @@ class Pizza(ABC):
     @abstractmethod
     def clam_pizza():
         pass
-    
+
     @abstractmethod
     def veggie_pizza():
         pass
 
-class Chicago_Pizza(Pizza):
-    def __init__(self,pizza_type):
-        self.pizza_type=pizza_type
+
+class ChicagoPizza(Pizza):
+    def __init__(self, pizza_type):
+        self.pizza_type = pizza_type
 
     def __call__(self):
 
-        if self.pizza_type=='Cheese':
+        if self.pizza_type == "Cheese":
             return cheese_pizza()
 
-        elif self.pizza_type=='Pepperoni':
+        elif self.pizza_type == "Pepperoni":
             return pepperoni_pizza()
 
-        elif self.pizza_type=='Clam':
+        elif self.pizza_type == "Clam":
             return clam_pizza()
-       
-        elif self.pizza_type=='Veggie':
+
+        elif self.pizza_type == "Veggie":
             return veggie_pizza()
 
-      
-        def cheese_pizza():
-            pass
+        else:
+            return "We dont serve this pizza"
 
-       
-        def pepperoni_pizza():
-            pass
+    def cheese_pizza():
+        return "Chicago cheese pizza"
 
-       
-        def clam_pizza():
-            pass
+    def pepperoni_pizza():
+        return "Chicago pepperoni pizza"
 
-        
-        def veggie_pizza():
-            pass
+    def clam_pizza():
+        return "Chicago clam pizza "
+
+    def veggie_pizza():
+        return "Chicago veggie pizza"
+
+
+class NYPizza(Pizza):
+    def __init__(self, pizza_type):
+        self.pizza_type = pizza_type
+
+    def __call__(self):
+
+        if self.pizza_type == "Cheese":
+            return self.cheese_pizza()
+
+        elif self.pizza_type == "Pepperoni":
+            return self.pepperoni_pizza()
+
+        elif self.pizza_type == "Clam":
+            return self.clam_pizza()
+
+        elif self.pizza_type == "Veggie":
+            return self.veggie_pizza()
+        else:
+            return "We dont serve this pizza type"
+
+    @staticmethod
+    def cheese_pizza():
+        return "NY cheese pizza"
+
+    @staticmethod
+    def pepperoni_pizza():
+        return "NY pepperoni pizza"
+
+    @staticmethod
+    def clam_pizza():
+        return "NY clam pizza "
+
+    @staticmethod
+    def veggie_pizza():
+        return "NY veggie pizza"
 
